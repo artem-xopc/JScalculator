9214e6e535b9e87d0090b2c817932c2f3084cce9
artem-xopc <romanovartem.2000@mail.ru>
2023-01-09 18:39:08 +0300

    полностью изменённая логика программы

--- index.js
+++ index.js
@@ -1,86 +1,78 @@
-const getHistory = () => {
-  return document.getElementById("history__value").innerText;
+let firstNum = "";
+let secondNum = "";
+let sign = "";
+let finish = false;
+
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const operator = ["+", "-", "/", "*"];
+
+// выводим экран калькулятора
+const output = document.querySelector(".input p");
+
+// функция очищения экрана
+const clearAll = () => {
+  firstNum = "";
+  secondNum = "";
+  sign = "";
+  finish = false;
+  output.textContent = 0;
 };
 
-const printHistory = (num) => {
-  document.getElementById("history__value").innerText = num;
-};
+document.querySelector(".btn_all").onclick = (event) => {
+  if (!event.target.classList.contains("btn")) return; // если нажата не кнопка, то мы обрываем выполнение
+  if (event.target.classList.contains("clear")) clearAll(); // если нажата кнопка "С", то мы вызываем функцию clearAll
 
-const getOutput = () => {
-  return document.getElementById("output__value");
-};
+  output.textContent = "0";
 
-const printOutput = (num) => {
-  if (num == "") {
-    document.getElementById("output__value").innerText = num;
-  } else {
-    document.getElementById("output__value").innerText = getFormatNum(num);
+  const key = event.target.textContent;
+
+  if (digit.includes(key)) {
+    if (secondNum === "" && sign === "") {
+      firstNum += key;
+      output.textContent = firstNum;
+    } else if (firstNum !== "" && secondNum !== "" && finish) {
+        secondNum = key
+        finish = false
+        output.textContent = secondNum
+    } else {
+      secondNum += key;
+      output.textContent = secondNum;
+    }
+    console.log(firstNum, secondNum, sign);
+    return;
   }
-};
 
-const getFormatNum = (num) => {
-  if (num == "_") {
-    return "";
+  if (operator.includes(key)) {
+    sign = key;
+    output.textContent = sign;
+    console.log(sign);
+    return;
   }
-  let numb = Number(num);
-  let value = numb.toLocaleString("en");
-  return value;
-};
 
-const reverseNum = (num) => {
-  return Number(num.replace(/,/g, ""));
-};
-
-const calculatorHistory = () => {
-  let operator = document.getElementsByClassName("operator");
-
-  for (let i = 0; i < operator.length; i++) {
-    operator[i].addEventListener("click", function () {
-      if (this.id == "clear") {
-        printHistory("");
-        printOutput("");
-      } else if (this.id == "backspace") {
-        let output = reverseNum(getOutput()).toString();
-        if (output) {
-          output = output.substring(0, output.length - 1);
-          printOutput(output);
+  if (key === "=") {
+    if (secondNum === '') secondNum = firstNum
+    switch (sign) {
+      case "+":
+        firstNum = (+firstNum) + (+secondNum);
+        break;
+      case "-":
+        firstNum = firstNum - secondNum;
+        break;
+      case "*":
+        firstNum = firstNum * secondNum;
+        break;
+      case "/":
+        if(secondNum === "0") {
+            output.textContent = "Ошибка"
+            firstNum = ""
+            secondNum = ""
+            sign = ""
+            return;
         }
-      } else {
-        let output = getOutput();
-        let history = getHistory();
-        if (output == "" && history != "") {
-          if (isNaN(history[history.length - 1])) {
-            history = history.substring(0, history.length - 1);
-          }
-        }
-        if (output != "" || history != "") {
-          output = output == "" ? output : reverseNum(output);
-          history = history + output;
-          if (this.id == "=") {
-            let result = eval(history);
-            printOutput(result);
-            printHistory("");
-          } else {
-            history = history + this.id;
-            printHistory(history);
-            printOutput("");
-          }
-        }
-      }
-    });
-  }
-};
-
-const calculatorNumber = () => {
-  let number = document.getElementsByClassName("number");
-
-  for (i = 0; i <= number.length; i++) {
-    number[i].addEventListener("click", function () {
-      let output = reverseNum(getOutput());
-      if (output != NaN) {
-        output = output + this.id;
-        printOutput(output);
-      }
-    });
+        firstNum = firstNum / secondNum;
+        break;
+    }
+    finish = true;
+    output.textContent = firstNum
   }
 };
